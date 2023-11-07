6031faf8de564c9c1b1178faaa97ba1a7ef72e6e
Draexx <p.carranza.is@gmail.com>
2023-11-07 14:01:19 -0400

    Update inicio.php

---
+++ 01-Introduccion/inicio.php
@@ -0,0 +1,30 @@
+<?php
+/*
+primer archivo 
+realizado con php
+*/
+
+$mi_variable = "hola mundo \n";
+$miVariable = "hello world";
+echo $mi_variable;
+$numero = 12;
+$float = 12.5; // es de tipo entero
+echo gettype($mi_variable) . "\n";
+echo gettype($numero); 
+echo "<hr>";
+echo "<br/>";
+
+$array = array();
+$array = [1,2,6,6];
+print_r($array);
+
+if ($miVariable == "hello world"){
+    echo "<br/>";
+    echo $mi_variable;
+}
+
+foreach ($array as $key =>$value){
+    echo $key."=>".$value."\n";
+}
+
+?>
